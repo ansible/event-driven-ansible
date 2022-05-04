12a809c78d903de891a98775e256e7d40e887436
Ben Thomasson <bthomass@redhat.com>
2022-05-04 12:26:26 -0400

    Adds range event source plugin

---
+++ benthomasson/eda/plugins/event_source/range.py
@@ -0,0 +1,30 @@
+"""
+range.py
+
+An ansible-events event source plugin for generating events with an increasing index i.
+
+Arguments:
+    limit: The upper limit of the range of the index.
+
+Example:
+
+    - name: range
+      range:
+        limit: 5
+
+"""
+
+
+def main(queue, args):
+
+    for i in range(int(args["limit"])):
+        queue.put(dict(i=i))
+
+
+if __name__ == "__main__":
+
+    class MockQueue:
+        def put(self, event):
+            print(event)
+
+    main(MockQueue(), dict(limit=5))
