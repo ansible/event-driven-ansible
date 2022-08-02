3491cc87dbd6693865a6b59443f332247c22453b
josephpisciotta <josephpisciotta@mac.com>
2022-08-02 02:24:41 -0400

    Fix url_check to use asyncio

--- benthomasson/eda/plugins/event_source/url_check.py
+++ benthomasson/eda/plugins/event_source/url_check.py
@@ -20,12 +20,12 @@ Example:
         delay: 10
 """
 
-import requests
-import time
-from requests.exceptions import ConnectionError
+import asyncio
+import aiohttp
+from typing import Any, Dict
 
 
-def main(queue, args):
+async def main(queue: asyncio.Queue, args: Dict[str, Any]):
 
     urls = args.get('urls', [])
     delay = args.get('delay', 1)
@@ -34,28 +34,21 @@ def main(queue, args):
         return
 
     while True:
-
+      async with aiohttp.ClientSession() as session:
         for url in urls:
-            try:
-                response = requests.get(url)
-                queue.put(dict(url_check=dict(url=url,
-                                              status='up' if response.status_code == 200 else 'down',
-                                              status_code=response.status_code)))
+                async with session.get(url) as resp:
+                  await queue.put(dict(url_check=dict(url=url,
+                                              status='up' if resp.status == 200 else 'down',
+                                              status_code=resp.status)))
 
-            except ConnectionError:
-                queue.put(dict(url_check=dict(url=url,
-                                              status='down',
-                                              status_code=None)))
-        time.sleep(delay)
+        await asyncio.sleep(delay)
 
 
 
 
 if __name__ == "__main__":
     class MockQueue:
-        def put(self, event):
+        async def put(self, event):
             print(event)
 
-    main(MockQueue(), {'urls': ['http://redhat.com']})
-
-
+    asyncio.run(main(MockQueue(), {'urls': ['http://redhat.com']}))
