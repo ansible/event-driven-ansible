7d55a06770cdbf197ff88441e6a4c448e4baa734
Ben Thomasson <bthomass@redhat.com>
2022-08-05 13:21:41 -0400

    Adds tick source

---
+++ benthomasson/eda/plugins/event_source/tick.py
@@ -0,0 +1,34 @@
+"""
+tick.py
+
+An ansible-events event source plugin for generating events with an increasing index i that never ends.
+
+Arguments:
+    delay: time between ticks
+
+Example:
+
+    - benthomasson.eda.tick:
+        delay: 5
+
+"""
+import asyncio
+import itertools
+from typing import Any, Dict
+
+
+async def main(queue: asyncio.Queue, args: Dict[str, Any]):
+    delay = args.get("delay", 1)
+
+    for i in itertools.count(start=1):
+        await queue.put(dict(i=i))
+        await asyncio.sleep(delay)
+
+
+if __name__ == "__main__":
+
+    class MockQueue:
+        async def put(self, event):
+            print(event)
+
+    asyncio.run(main(MockQueue(), dict(delay=1)))
