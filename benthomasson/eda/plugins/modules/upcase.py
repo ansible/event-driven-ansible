91822b87b96eda8c5bf742d021f4c964e6cf7c9a
hsong-rh <hsong@redhat.com>
2022-08-10 14:12:05 -0400

    Removed incorrect license information

--- benthomasson/eda/plugins/modules/upcase.py
+++ benthomasson/eda/plugins/modules/upcase.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python
 
-# This file is part of Ansible
-#
 # Ansible is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
