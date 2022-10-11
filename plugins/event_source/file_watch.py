74f33995db8f6d84713e91d0967048181ff654e0
Ben Thomasson <bthomass@redhat.com>
2022-10-11 14:28:24 -0400

    Updates file watching plugin to use async entrypoint

--- plugins/event_source/file_watch.py
+++ plugins/event_source/file_watch.py
@@ -1,5 +1,5 @@
 """
-watchdog.py
+file_watch.py
 
 An ansible-rulebook event source plugin for watching file system changes.
 
@@ -10,68 +10,74 @@ Arguments:
 
 Example:
 
-    - ansible.eda.watchdog:
+    - name: file_watch
+      file_watch:
         path: "{{src_path}}"
         recursive: true
-        ignore_regexes: ['.*.pytest.*', '.*__pycache__.*', '.*/.git.*']
+        ignore_regexes: ['.*\.pytest.*', '.*__pycache__.*', '.*/.git.*']
 """
 
-import os
+import asyncio
+import concurrent.futures
 
 from watchdog.events import RegexMatchingEventHandler
 from watchdog.observers import Observer
 
 
-def main(queue, args):
+def watch(loop, queue, args):
 
-    root_path = os.path.abspath(args["path"])
+    root_path = args["path"]
 
     class Handler(RegexMatchingEventHandler):
         def __init__(self, **kwargs):
             RegexMatchingEventHandler.__init__(self, **kwargs)
 
         def on_created(self, event):
-            queue.put(
+            loop.call_soon_threadsafe(
+                queue.put_nowait,
                 dict(
                     change="created",
                     src_path=event.src_path,
                     type=event.__class__.__name__,
                     root_path=root_path,
-                )
+                ),
             )
 
         def on_deleted(self, event):
-            queue.put(
+            loop.call_soon_threadsafe(
+                queue.put_nowait,
                 dict(
                     change="deleted",
                     src_path=event.src_path,
                     type=event.__class__.__name__,
                     root_path=root_path,
-                )
+                ),
             )
 
         def on_modified(self, event):
-            queue.put(
+            loop.call_soon_threadsafe(
+                queue.put_nowait,
                 dict(
                     change="modified",
                     src_path=event.src_path,
                     type=event.__class__.__name__,
                     root_path=root_path,
-                )
+                ),
             )
 
         def on_moved(self, event):
-            queue.put(
+            loop.call_soon_threadsafe(
+                queue.put_nowait,
                 dict(
                     change="moved",
                     src_path=event.src_path,
                     type=event.__class__.__name__,
                     root_path=root_path,
-                )
+                ),
             )
 
     observer = Observer()
-    handler = Handler(ignore_regexes=args.get("ignore_regexes"))
+    handler = Handler(ignore_regexes=args.get("ignore_regexes", []))
     observer.schedule(handler, root_path, recursive=args["recursive"])
     observer.start()
 
@@ -80,3 +86,19 @@ def main(queue, args):
     finally:
         observer.stop()
         observer.join()
+
+
+async def main(queue, args):
+    loop = asyncio.get_event_loop()
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=1) as task_pool:
+        await loop.run_in_executor(task_pool, watch, loop, queue, args)
+
+
+if __name__ == "__main__":
+
+    class MockQueue:
+        def put_nowait(self, event):
+            print(event)
+
+    asyncio.run(main(MockQueue(), {"path": "/tmp", "recursive": True}))
