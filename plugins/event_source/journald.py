ab3e13260cfed64ba577e2c5d409ed4825801ed8
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2022-10-12 14:50:33 +0200

    add event source for systemd journald logs
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>

---
+++ plugins/event_source/journald.py
@@ -0,0 +1,44 @@
+"""
+journald.py
+An ansible-events event source plugin that tails systemd journald logs.
+
+Arguments:
+    match - return messages that matches this field, see https://www.freedesktop.org/software/systemd/man/systemd.journal-fields.html
+
+Examples:
+    - name: Return severity 6 messages
+      ansible.eda.journald:
+        match: "PRIORITY=6"
+
+    - name: Return messages when sudo is used
+      become: true
+      ansible.eda.journald:
+        match: "_EXE=/usr/bin/sudo"
+"""
+
+import asyncio
+from typing import Any, Dict
+from systemd import journal
+
+
+async def main(queue: asyncio.Queue, args: Dict[str, Any]):
+    match = args.get("match", [])
+
+    if not match:
+        return
+
+    journal_stream = journal.Reader()
+
+    while True:
+        journal_stream.add_match(match)
+        for entry in journal_stream:
+            await queue.put(entry)
+
+
+if __name__ == "__main__":
+
+    class MockQueue:
+        async def put(self, event):
+            print(event)
+
+    asyncio.run(main(MockQueue(), {"match": "PRIORITY=6"}))
