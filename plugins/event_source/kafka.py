609801d63904bd1805a1a0fbac18ee75c30e31b9
morvencao <lcao@redhat.com>
2022-12-13 10:07:11 +0800

    add ssl support for kafka source.
    Signed-off-by: morvencao <lcao@redhat.com>

--- plugins/event_source/kafka.py
+++ plugins/event_source/kafka.py
@@ -6,6 +6,12 @@ An ansible-rulebook event source plugin for receiving events via a kafka topic.
 Arguments:
     host:      The host where the kafka topic is hosted
     port:      The port where the kafka server is listening
+    cafile     The optional certificate authority file path containing certificates used to
+               sign kafka broker certificates
+    certfile   The optional client certificate file path containing the client certificate,
+               as well as CA certificates needed to establish the certificate's authenticity
+    keyfile    The optional client key file path containing the client private key
+    password   The optional password to be used when loading the certificate chain
     topic:     The kafka topic
     group_id:  A kafka group id
     offset:    Where to automatically reset the offset. [latest, earliest]
@@ -19,6 +25,7 @@ import asyncio
 import json
 import logging
 from aiokafka import AIOKafkaConsumer
+from aiokafka.helpers import create_ssl_context
 from typing import Any, Dict
 
 
@@ -28,6 +35,10 @@ async def main(queue: asyncio.Queue, args: Dict[str, Any]):
     topic = args.get("topic")
     host = args.get("host")
     port = args.get("port")
+    cafile = args.get("cafile")
+    certfile = args.get("certfile")
+    keyfile = args.get("keyfile")
+    password = args.get("password")
     group_id = args.get("group_id", None)
     offset = args.get("offset", "latest")
 
@@ -42,6 +53,17 @@ async def main(queue: asyncio.Queue, args: Dict[str, Any]):
         max_poll_records=1,
         auto_offset_reset=offset,
     )
+
+    if cafile:
+        context = create_ssl_context(
+            cafile=cafile,
+            certfile=certfile,
+            keyfile=keyfile,
+            password=password,
+        )
+        kafka_consumer.security_protocol = "SSL"
+        kafka_consumer.ssl_context = context
+
     await kafka_consumer.start()
 
     try:
