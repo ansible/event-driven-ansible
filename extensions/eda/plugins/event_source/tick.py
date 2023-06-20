fc3bb7b6d90210aa18a6c7556923dc5df27997e3
Erik Clarizio <eclarizio@gmail.com>
2023-06-20 09:50:34 -0700

    Fix some ruff issues in tick.py
    Remaining issues:
    36:23: ANN101 Missing type annotation for `self` in method

--- extensions/eda/plugins/event_source/tick.py
+++ extensions/eda/plugins/event_source/tick.py
@@ -18,7 +18,8 @@ import itertools
 from typing import Any
 
 
-async def main(queue: asyncio.Queue, args: dict[str, Any]):
+async def main(queue: asyncio.Queue, args: dict[str, Any]) -> None:
+    """Generate events with an increasing index i and a time between ticks."""
     delay = args.get("delay", 1)
 
     for i in itertools.count(start=1):
@@ -27,9 +28,13 @@ async def main(queue: asyncio.Queue, args: dict[str, Any]):
 
 
 if __name__ == "__main__":
+    """MockQueue if running directly."""
 
     class MockQueue:
-        async def put(self, event):
-            print(event)
+        """A fake queue."""
+
+        async def put(self, event: dict) -> None:
+            """Print the event."""
+            print(event) # noqa: T201
 
     asyncio.run(main(MockQueue(), {"delay": 1}))
