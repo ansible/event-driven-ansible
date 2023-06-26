1e64fa418cae5452343f26028db20059baf39b77
Bill Wei <bilwei@redhat.com>
2023-06-26 16:12:50 -0400

    Ignore UP038 because python3.9 does not support it

--- extensions/eda/plugins/event_filter/insert_hosts_to_meta.py
+++ extensions/eda/plugins/event_filter/insert_hosts_to_meta.py
@@ -49,7 +49,7 @@ def main(
 
     if isinstance(hosts, str):
         hosts = hosts.split(host_separator) if host_separator else [hosts]
-    elif isinstance(hosts, list | tuple):
+    elif isinstance(hosts, (list, tuple)):  # noqa: UP038
         for h in hosts:
             if not isinstance(h, str):
                 msg = f"{h} is not a valid hostname"
