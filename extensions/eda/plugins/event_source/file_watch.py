c9542d8c36d039bffe9514730def4d05f35812be
Tom Tuffin <ttuffin@redhat.com>
2023-07-04 19:31:07 +0200

    Fix typehint error in filewatch plugin

--- extensions/eda/plugins/event_source/file_watch.py
+++ extensions/eda/plugins/event_source/file_watch.py
@@ -20,7 +20,6 @@ Example:
 
 import asyncio
 import concurrent.futures
-from typing import Optional
 
 from watchdog.events import RegexMatchingEventHandler
 from watchdog.observers import Observer
@@ -35,7 +34,7 @@ def watch(
     root_path = args["path"]
 
     class Handler(RegexMatchingEventHandler):
-        def __init__(self: "Handler", **kwargs: Optional(list[str])) -> None:
+        def __init__(self: "Handler", **kwargs: dict) -> None:
             RegexMatchingEventHandler.__init__(self, **kwargs)
 
         def on_created(self: "Handler", event: dict) -> None:
