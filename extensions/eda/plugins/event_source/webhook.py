6d04a6af696c9b500b09834787c97ffe33a821ec
Laszlo Szomor <laszomor@gmail.com>
2023-08-06 19:56:52 +0200

    Add HMAC verification support to webhook plugin

--- extensions/eda/plugins/event_source/webhook.py
+++ extensions/eda/plugins/event_source/webhook.py
@@ -12,10 +12,21 @@ Arguments:
     certfile: The optional path to a certificate file to enable TLS support
     keyfile:  The optional path to a key file to be used together with certfile
     password: The optional password to be used when loading the certificate chain
+    hmac_secret: The optional HMAC secret used to verify the payload from the client
+    hmac_algo: The optional HMAC algorithm used to calculate the payload hash.
+               See your python's hashlib.algorithms_available set for available options.
+               Defaults to sha256
+    hmac_header: The optional HMAC header sent by the client with the payload signature.
+                 Defaults to x-hub-signature-256
+    hmac_format: The optional HMAC signature format format. Supported formats: hex, base64
+                 Defaults to hex
 
 """
 
 import asyncio
+import base64
+import hashlib
+import hmac
 import json
 import logging
 import ssl
@@ -56,6 +67,36 @@ def _parse_token(request: web.Request) -> (str, str):
     return scheme, token
 
 
+async def _hmac_verify(request: web.Request) -> bool:
+    result = False
+
+    hmac_secret = request.app["hmac_secret"]
+    hmac_header = request.app["hmac_header"]
+    hmac_algo = request.app["hmac_algo"]
+    hmac_format = request.app["hmac_format"]
+
+    if hmac_header not in request.headers:
+        raise web.HTTPBadRequest(text="Signature header not found")
+
+    hmac_header_digest = request.headers[hmac_header].strip()
+
+    if hmac_header_digest.startswith(f"{hmac_algo}="):
+        hmac_header_digest = hmac_header_digest[len(f"{hmac_algo}="):]
+
+    body = await request.text()
+
+    event_hmac = hmac.new(key=hmac_secret,
+                          msg=body.encode("utf-8"), digestmod=hmac_algo)
+    if hmac_format == "base64":
+        event_digest = base64.b64encode(event_hmac.digest()).decode("utf-8")
+    elif hmac_format == "hex":
+        event_digest = event_hmac.hexdigest()
+
+    result = hmac.compare_digest(hmac_header_digest, event_digest)
+
+    return result
+
+
 @web.middleware
 async def bearer_auth(request: web.Request, handler: Callable) -> web.StreamResponse:
     """Verify authorization is Bearer type."""
@@ -69,16 +110,46 @@ async def bearer_auth(request: web.Request, handler: Callable) -> web.StreamResp
     return await handler(request)
 
 
+@web.middleware
+async def hmac_verify(request: web.Request, handler: Callable) -> web.StreamResponse:
+    """Verify event's HMAC signature."""
+    hmac_verified = await _hmac_verify(request)
+    if not hmac_verified:
+        raise web.HTTPUnauthorized(text="HMAC verification failed")
+
+    return await handler(request)
+
+
 async def main(queue: asyncio.Queue, args: dict[str, Any]) -> None:
     """Receive events via webhook."""
     if "port" not in args:
         msg = "Missing required argument: port"
         raise ValueError(msg)
+
+    middlewares = []
+    app_attrs = {}
+
     if "token" in args:
-        app = web.Application(middlewares=[bearer_auth])
-        app["token"] = args["token"]
-    else:
-        app = web.Application()
+        middlewares.append(bearer_auth)
+        app_attrs["token"] = args["token"]
+
+    if "hmac_secret" in args:
+        middlewares.append(hmac_verify)
+
+        app_attrs["hmac_secret"] = args["hmac_secret"].encode("utf-8")
+        app_attrs["hmac_algo"] = args.get("hmac_algo", "sha256")
+        if app_attrs["hmac_algo"] not in hashlib.algorithms_available:
+            msg = f"Unsupported HMAC algorithm: {app_attrs['hmac_algo']}"
+            raise ValueError(msg)
+        app_attrs["hmac_header"] = args.get("hmac_header", "x-hub-signature-256")
+        app_attrs["hmac_format"] = args.get("hmac_format", "hex")
+        if app_attrs["hmac_format"] not in ["hex", "base64"]:
+            msg = f"Unsupported HMAC header format {app_attrs['hmac_format']}"
+            raise ValueError(msg)
+
+    app = web.Application(middlewares=middlewares)
+    for (key, value) in app_attrs.items():
+        app[key] = value
     app["queue"] = queue
 
     app.add_routes(routes)
