bd021fb3e44bb4be266073b4daa614b0b5115673
Ritesh Shah <9796427+ritzshah@users.noreply.github.com>
2023-09-14 16:57:24 +0530

    Update kafka_scram.py

--- extensions/eda/plugins/event_source/kafka_scram.py
+++ extensions/eda/plugins/event_source/kafka_scram.py
@@ -26,57 +26,54 @@ import asyncio
 import json
 import logging
 from typing import Any
+from kafka import KafkaConsumer
+import ssl
 
-from aiokafka import AIOKafkaConsumer
-from aiokafka.helpers import create_ssl_context
-
-
-async def main(  # pylint: disable=R0914
+async def main(
     queue: asyncio.Queue,
     args: dict[str, Any],
 ) -> None:
-    """Receive events via a kafka topic."""
+    """Receive events via a Kafka topic."""
     logger = logging.getLogger()
 
     topic = args.get("topic")
     host = args.get("host")
     port = args.get("port")
-    username  = args.get("username")
+    username = args.get("username")
     password = args.get("password")
     security_protocol = args.get("security_protocol")
     sasl_mechanism = args.get("sasl_mechanism")
-    offset = args.get("offset", "latest")    
 
-# EXAMPLE
-#    topic = "reviews.sentiment"
-#    host = "kafka-kafka-bootstrap.globex-mw.svc.cluster.local"
-#    port = "9092"
-#    username  = "globex"
-#    password = "globex"
-#    security_protocol = "SASL_PLAINTEXT"
-#    sasl_mechanism = "SCRAM-SHA-512"
-
-    if offset not in ("latest", "earliest"):
-        msg = f"Invalid offset option: {offset}"
+    if security_protocol not in ("SASL_PLAINTEXT", "SASL_SSL"):
+        msg = f"Invalid security_protocol option: {security_protocol}"
         raise ValueError(msg)
 
-    kafka_consumer = AIOKafkaConsumer(
+    if sasl_mechanism not in ("PLAIN", "GSSAPI", "SCRAM-SHA-256", "SCRAM-SHA-512", "OAUTHBEARER"):
+        msg = f"Invalid sasl_mechanism option: {sasl_mechanism}"
+        raise ValueError(msg)
+
+    if security_protocol == "SASL_SSL":
+        ssl_context = ssl.create_default_context()
+        ssl_context.check_hostname = False
+        ssl_context.verify_mode = ssl.CERT_NONE
+        ssl_context.set_ciphers("DEFAULT@SECLEVEL=1")
+
+    kafka_consumer = KafkaConsumer(
         topic,
         bootstrap_servers=f"{host}:{port}",
         sasl_plain_username=username,
         sasl_plain_password=password,
         security_protocol=security_protocol,
         sasl_mechanism=sasl_mechanism,
-        auto_offset_reset=offset,      
+        ssl_context=ssl_context if security_protocol == "SASL_SSL" else None,
+        auto_offset_reset="latest",  # You can change this as needed
     )
 
-    await kafka_consumer.start()
-
     try:
-        async for msg in kafka_consumer:
+        for msg in kafka_consumer:
             data = None
             try:
-                value = msg.value.decode(encoding)
+                value = msg.value.decode("utf-8")  # Specify encoding if different
                 print(value)
                 data = json.loads(value)
             except json.decoder.JSONDecodeError:
@@ -88,8 +85,8 @@ async def main(  # pylint: disable=R0914
                 await queue.put({"body": data})
             await asyncio.sleep(0)
     finally:
-        logger.info("Stopping kafka consumer")
-        await kafka_consumer.stop()
+        logger.info("Stopping Kafka consumer")
+        kafka_consumer.close()
 
 
 if __name__ == "__main__":
@@ -98,13 +95,21 @@ if __name__ == "__main__":
     class MockQueue:
         """A fake queue."""
 
-        async def put(self: "MockQueue", event: dict) -> None:
+        async def put(self, event: dict) -> None:
             """Print the event."""
             print(event)  # noqa: T201
 
     asyncio.run(
         main(
             MockQueue(),
-            {"topic": "eda", "host": "localhost", "port": "9092", "group_id": "test"},
-        ),
+            {
+                "topic": "reviews.sentiment",
+                "host": "kafka-kafka-bootstrap.globex-mw.svc.cluster.local",
+                "port": "9092",
+                "sasl_mechanism": "SCRAM-SHA-512",
+                "username": "globex",
+                "password": "globex",
+                "security_protocol": "SASL_PLAINTEXT",  # Adjust as needed
+            },
+        )
     )
