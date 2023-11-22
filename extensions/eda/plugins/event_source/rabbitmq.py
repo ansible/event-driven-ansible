55416458cc35f4b8551307c22a1ae13da79143b9
Sean M. Collins <sean@coreitpro.com>
2023-11-22 09:23:16 -0500

    Basic implementation

--- extensions/eda/plugins/event_source/rabbitmq.py
+++ extensions/eda/plugins/event_source/rabbitmq.py
@@ -6,10 +6,12 @@ Arguments:
 ---------
     host:      The host where rabbitmq is hosted
     port:      The port where the rabbitmq server is listening
+    queue:     The queue name to listen for messages on
 
 """
 
 import asyncio
+import aiorabbit
 import json
 import logging
 from typing import Any
@@ -18,3 +20,42 @@ from typing import Any
 async def main(queue: asyncio.Queue, args: dict[str, Any]) -> None:
     """Receive events via a rabbitmq topic."""
     logger = logging.getLogger()
+
+    host = args.get("host")
+    port = args.get("port")
+    queue_name = args.get("queue")
+
+    url = f"amqp://{host}:{port}"
+
+    async with aiorabbit.connect(url) as client:
+        await client.queue_declare(queue_name)
+        logger.info('Consuming from %s', queue)
+        async for msg in client.consume(queue_name):
+            data = None
+            logger.debug('Received message published to %s: %r',
+                         queue_name, msg.body)
+            try:
+                data = json.loads(msg.body)
+            except json.decoder.JSONDecodeError:
+                logger.exception("JSON parse error")
+
+            if data:
+                await queue.put({"body": data})
+            await asyncio.sleep(0)
+
+if __name__ == "__main__":
+    """MockQueue if running directly."""
+
+    class MockQueue:
+        """A fake queue."""
+
+        async def put(self: "MockQueue", event: dict) -> None:
+            """Print the event."""
+            print(event)  # noqa: T201
+
+    asyncio.run(
+        main(
+            MockQueue(),
+            {"host": "localhost", "port": "9092", "queue": "test"},
+        ),
+    )
