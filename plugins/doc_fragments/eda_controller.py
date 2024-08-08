2b60f0e0d47b17fbf4a4bdf336fece0266bc110f
Abhijeet Kasurde <akasurde@redhat.com>
2024-08-08 08:57:13 -0700

    CI fixes
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

--- plugins/doc_fragments/eda_controller.py
+++ plugins/doc_fragments/eda_controller.py
@@ -3,8 +3,6 @@
 # Copyright: Contributors to the Ansible project
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-from __future__ import annotations
-
 
 class ModuleDocFragment:
 
--- plugins/module_utils/errors.py
+++ plugins/module_utils/errors.py
@@ -3,7 +3,6 @@
 # Copyright: Contributors to the Ansible project
 # Simplified BSD License (see licenses/simplified_bsd.txt or https://opensource.org/licenses/BSD-2-Clause)
 
-from __future__ import annotations
 from __future__ import (absolute_import, division, print_function)
 
 __metaclass__ = type
