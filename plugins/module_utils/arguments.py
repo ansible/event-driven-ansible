5255d20481e90e05039e970f91647373d3bb59df
Abhijeet Kasurde <akasurde@redhat.com>
2024-08-08 08:53:33 -0700

    CI fixes
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

--- plugins/module_utils/arguments.py
+++ plugins/module_utils/arguments.py
@@ -3,7 +3,8 @@
 # Copyright: Contributors to the Ansible project
 # Simplified BSD License (see licenses/simplified_bsd.txt or https://opensource.org/licenses/BSD-2-Clause)
 
-from __future__ import absolute_import, annotations, division, print_function
+from __future__ import annotations
+from __future__ import (absolute_import, division, print_function)
 
 __metaclass__ = type
 
