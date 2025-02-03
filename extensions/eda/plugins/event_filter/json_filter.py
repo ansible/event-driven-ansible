9a1588592ed1770bd1f330d33a57d547cf7969b3
Doston <31990136+Dostonbek1@users.noreply.github.com>
2025-02-03 14:33:07 -0500

    feat: [AAP-38755] add documentation for event filter plugins (#388)

--- extensions/eda/plugins/event_filter/json_filter.py
+++ extensions/eda/plugins/event_filter/json_filter.py
@@ -1,22 +1,55 @@
-"""json_filter.py:   An event filter that filters keys out of events.
-
-Includes override excludes.
-
-This is useful to exclude information from events that is unneeded by the rule
-engine.
-
-Arguments:
----------
-    * exclude_keys = a list of strings or patterns to remove
-    * include_keys = a list of strings or patterns to keep even if it matches
-    exclude_keys patterns.
-
-"""
-
 from __future__ import annotations
 
 import fnmatch
 from typing import Any, Optional
+
+DOCUMENTATION = r"""
+---
+author:
+  - Doston Toirov (@dtoirov)
+short_description: Filter keys out of events.
+description:
+  - An event filter that filters keys out of events.
+    Includes override excludes.
+    This is useful to exclude information from events that is unneeded by the rule engine.
+options:
+  exclude_keys:
+    description:
+      - A list of strings or patterns to remove.
+    type: list
+    elements: str
+    default: null
+  include_keys:
+    description:
+      - A list of strings or patterns to keep even if it matches exclude_keys patterns.
+    type: list
+    elements: str
+    default: null
+notes:
+  - The values in both parameters - include_keys and exclude_keys, must be a full path in
+    top to bottom order to the keys to be filtered (or left to right order if it is given
+    as a list), as shown in the examples below.
+"""
+
+EXAMPLES = r"""
+- ansible.eda.generic:
+    payload:
+      key1:
+        key2:
+          f_ignore_1: 1
+          f_ignore_2: 2
+      key3:
+        key4:
+          f_use_1: 42
+          f_use_2: 45
+  filters:
+    - ansible.eda.json_filter:
+        include_keys:
+          - key3
+          - key4
+          - f_use*
+        exclude_keys: ['key1', 'key2', 'f_ignore_1']
+"""
 
 
 def _matches_include_keys(include_keys: list[str], string: str) -> bool:
--- extensions/eda/plugins/event_filter/noop.py
+++ extensions/eda/plugins/event_filter/noop.py
@@ -1,6 +1,13 @@
-"""noop.py:   An event filter that does nothing to the input."""
-
 from typing import Any
+
+DOCUMENTATION = r"""
+---
+author:
+  - Doston Toirov (@dtoirov)
+short_description: Do nothing.
+description:
+  - An event filter that does nothing to the input.
+"""
 
 
 def main(event: dict[str, Any]) -> dict[str, Any]:
